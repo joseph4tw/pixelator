85a6fbe0ba45889562460eefc5497959092736e1
Joseph <joseph.serido@gmail.com>
2016-03-05 19:34:14 -0800

    Updated using directives

--- Pixelator/Pixelator.cs
+++ Pixelator/Pixelator.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using System.Drawing;
 using System.Threading.Tasks;
 using Microsoft.Office.Interop.Excel;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 
 namespace Pixelator
 {
